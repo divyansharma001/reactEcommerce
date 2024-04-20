9e2f0a58f3a4adaf9a8b2bb2063739bc0f309857
Divyansh Sharma <divyanshsharma4507@gmail.com>
2024-04-20 13:31:48 +0530

    Make carousel responsive

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,7 +1,7 @@
 
 function Carousel() {
   return (
-    <div className="carousel w-full" style={{height: "500px"}}>
+    <div className="carousel w-full h-500">
     <div id="slide1" className="carousel-item relative w-full">
       <img src="./images/cover6.png" className="w-full " />
       <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
